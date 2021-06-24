ea28c4e1e4d6e9100a967d7bfcdc1c0dc3f550d6
usuario <usuario@SERVIDOR>
2021-06-23 21:54:19 -0300

    Adicionadas Bibliotecas

--- src/Banco_dados/ClasseConexao.java
+++ src/Banco_dados/ClasseConexao.java
@@ -14,6 +14,7 @@ public class ClasseConexao {
 			if(conexao==null) {
 				// Vamos criar um string contendo o caminho do banco de dados:
 				String url = "jdbc:mysql://143.244.169.186/aluguel_Carros";
+				//String url = "jdbc:mysql://localhost/base_funcionarios";
 				
 				// Vamos fazer a coneção:
 				conexao = DriverManager.getConnection(url,"admin","43e3b9c1ea37bbd8c26ddd634bb74e2b6f048303744b83b2");
